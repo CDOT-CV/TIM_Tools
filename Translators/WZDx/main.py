9eda0e0dc9b0e28b8026518ae8e67bd1af6662b8
Brandon Payne <bpayne@trihydro.com>
2022-05-31 15:58:27 -0600

    Adding initial default SDX and RSU request objects

--- Translators/WZDx/main.py
+++ Translators/WZDx/main.py
@@ -1,6 +1,107 @@
 
 import sys
 import functions_framework
+import json
+import secrets
+
+
+def getBoundingBox(geometry):
+    # TODO: calculate bounding box and return
+    nwCorner = {"latitude": -90, "longitude": 180}
+    seCorner = {"latitude": 90, "longitude": -180}
+    # loop through coordinates and calculate max corners
+    # coordinates are stored longitude, latitude
+    for coords in geometry.get("coordinates"):
+        if coords[0] < nwCorner.get("longitude"):
+            nwCorner["longitude"] = coords[0]
+        if coords[1] > nwCorner.get("latitude"):
+            nwCorner["latitude"] = coords[1]
+        if coords[0] > seCorner.get("longitude"):
+            seCorner["longitude"] = coords[0]
+        if coords[1] < seCorner.get("latitude"):
+            seCorner["latitude"] = coords[1]
+    return nwCorner, seCorner
+
+
+def getSdwRequest(geometry):
+    nwCorner, seCorner = getBoundingBox(geometry)
+    sdx_req = {
+        "sdw": {
+            "ttl": "oneday",
+            "recordId": secrets.token_hex(4).upper(),
+            "serviceRegion": {
+                "nwCorner": {
+                    "latitude": nwCorner.get("latitude"),
+                    "longitude": nwCorner.get("longitude")
+                },
+                "seCorner": {
+                    "latitude": seCorner.get("latitude"),
+                    "longitude": seCorner.get("longitude")
+                }
+            }
+        }
+    }
+    return sdx_req
+
+
+def getRsusForMessage():
+    return [
+        {
+            "latitude": 40.0000000,
+            "longitude": -106.0000000,
+            "rsuId": 1,
+            "route": "Route",
+            "milepost": 100,
+            "rsuTarget": "10.10.10.10",
+            "rsuRetries": 3,
+            "rsuTimeout": 5000,
+            "rsuIndex": 2
+        }
+    ]
+
+
+def getDefaultSnmpSettings(feature):
+    return {
+        "rsuid": "83",
+        "msgid": 31,
+        "mode": 1,
+        "channel": 178,
+        "interval": 2,
+        "deliverystart": feature.get("properties").get("start_date"),
+        "deliverystop": feature.get("properties").get("end_date"),
+        "enable": 1,
+        "status": 4
+    }
+
+
+def getRsuRequest(feature):
+    tim_req = {
+        "rsus": getRsusForMessage(),
+        "snmp": getDefaultSnmpSettings(feature)
+    }
+    return tim_req
+
+
+def generateTim(wzdx_geojson):
+    return None
+
+
+def translate(wzdx_geojson):
+    tims = []
+    # TODO: generate two messages, one for sdx and one for rsu
+    for feature in wzdx_geojson.get("features"):
+        tim_body = generateTim(wzdx_geojson)
+        sdx_tim = {
+            "request": getSdwRequest(feature.get("geometry")),
+            "tim": tim_body
+        }
+        rsu_tim = {
+            "request": getRsuRequest(feature),
+            "tim": tim_body
+        }
+        tims.append(sdx_tim)
+        tims.append(rsu_tim)
+    return tims
 
 
 @functions_framework.http
@@ -21,15 +122,16 @@ def translateWzdxTIM(request):
     if request.method == 'OPTIONS':
         headers = {
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Methods': 'POST',
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Max-Age': '3600'
         }
         return ('', 204, headers)
 
     headers = {
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
     }
 
-    request_json = request.get_json()
-    return (request_json, 200, headers)
+    tims = translate(request.get_json())
+    return (json.dumps(tims), 200, headers)
