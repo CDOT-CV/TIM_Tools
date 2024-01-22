61b83942e5ab768904671b9d85599e9845b1bc76
Marc Wodahl <mwodahl@trihydro.com>
2024-01-22 08:05:27 -0700

    Update clear_index to use TIMDeleteController

--- Translators/WZDx/snmp_operations.py
+++ Translators/WZDx/snmp_operations.py
@@ -1,19 +1,23 @@
-import subprocess
-import logging
+import requests
+import json
 
 def clear_index(rsu):
-    '''Clears the specific RSU index'''
-    cmd = ''
-    index = rsu["rsuIndex"]
-    if rsu["snmpProtocol"] == "NTCIP1218":
-        cmd = f'snmpset -v 3 -u {rsu["rsuUsername"]} -a SHA -A {rsu["rsuPassword"]} -x AES -X {rsu["rsuPassword"]} -l authpriv {rsu["rsuTarget"]} 1.3.6.1.4.1.1206.4.2.18.3.2.1.9.{index} i 6'
+    deleteUrl = 'http://localhost:8080/tim'
+    headers = {'Content-Type': 'application/json'}
+    payload = json.dumps({
+        'rsuTarget': rsu['rsuTarget'],
+        'rsuRetries': rsu['rsuRetries'],
+        'rsuTimeout': rsu['rsuTimeout'],
+        'rsuIndex': rsu['rsuIndex'],
+        'snmpProtocol': rsu['snmpProtocol'],
+        'rsuUsername': rsu['rsuUsername'],
+        'rsuPassword': rsu['rsuPassword']
+    })
+    params = {'index': str(rsu['rsuIndex'])}
+    
+    response = requests.delete(deleteUrl, data=payload, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        print(f'Index {rsu["rsuIndex"]} cleared for RSU {rsu["rsuTarget"]}')
     else:
-        cmd = f'snmpset -v 3 -u {rsu["rsuUsername"]} -a SHA -A {rsu["rsuPassword"]} -x AES -X {rsu["rsuPassword"]} -l authpriv {rsu["rsuTarget"]} 1.0.15628.4.1.4.1.11.{index} i 6'
-    output = ''
-    try:
-        output = subprocess.run(cmd, shell=True, capture_output=True, check=True)
-        output = output.stdout.decode("utf-8").split('\n')[:-1]
-        print(f'index {index} cleared for RSU {rsu["rsuTarget"]}')
-    except subprocess.CalledProcessError as e:
-        output = e.stderr.decode("utf-8").split('\n')[:-1]
-        logging.error(f'Encountered error while running snmpset: {output[:-1]}')
+        print(f'Failed to clear index {rsu["rsuIndex"]} for RSU {rsu["rsuTarget"]}: {response.content.decode("utf-8")}')
--- tests/Translators/WZDx/test_snmp_operations.py
+++ tests/Translators/WZDx/test_snmp_operations.py
@@ -2,35 +2,47 @@ import unittest
 from unittest.mock import patch, Mock
 import Translators.WZDx.snmp_operations as snmp_operations
 
-
-@patch('snmp_operations.subprocess.run')
-def test_clear_index_1218(mock_run):
+@patch('Translators.WZDx.snmp_operations.requests.delete')
+def test_clear_index_1218(mock_delete):
     rsu = {
         "rsuIndex": "1",
         "snmpProtocol": "NTCIP1218",
         "rsuUsername": "username",
         "rsuPassword": "password",
-        "rsuTarget": "target"
+        "rsuTarget": "target",
+        "rsuRetries": 3,
+        "rsuTimeout": 10
     }
-    mock_run.return_value = Mock(stdout=b'output\n', stderr=b'', returncode=0)
+    mock_delete.return_value.status_code = 200
 
     snmp_operations.clear_index(rsu)
 
-    mock_run.assert_called_once()
-    mock_run.assert_called_with("snmpset -v 3 -u username -a SHA -A password -x AES -X password -l authpriv target 1.3.6.1.4.1.1206.4.2.18.3.2.1.9.1 i 6", shell=True, capture_output=True, check=True)
+    mock_delete.assert_called_once_with(
+        'http://localhost:8080/tim',
+        data='{"rsuTarget": "target", "rsuRetries": 3, "rsuTimeout": 10, "rsuIndex": "1", "snmpProtocol": "NTCIP1218", "rsuUsername": "username", "rsuPassword": "password"}',
+        headers={'Content-Type': 'application/json'},
+        params={'index': '1'}
+    )
 
-@patch('snmp_operations.subprocess.run')
-def test_clear_index_FOURDOT1(mock_run):
+@patch('Translators.WZDx.snmp_operations.requests.delete')
+def test_clear_index_FOURDOT1(mock_delete):
     rsu = {
         "rsuIndex": "1",
         "snmpProtocol": "FOURDOT1",
         "rsuUsername": "username",
         "rsuPassword": "password",
-        "rsuTarget": "target"
+        "rsuTarget": "target",
+        "rsuRetries": 3,
+        "rsuTimeout": 10
     }
-    mock_run.return_value = Mock(stdout=b'output\n', stderr=b'', returncode=0)
+    mock_delete.return_value.status_code = 200
 
-    snmp_operations.clear_index(rsu)
+    res = snmp_operations.clear_index(rsu)
 
-    mock_run.assert_called_once()
-    mock_run.assert_called_with("snmpset -v 3 -u username -a SHA -A password -x AES -X password -l authpriv target 1.0.15628.4.1.4.1.11.1 i 6", shell=True, capture_output=True, check=True)
+    mock_delete.assert_called_once_with(
+        'http://localhost:8080/tim',
+        data='{"rsuTarget": "target", "rsuRetries": 3, "rsuTimeout": 10, "rsuIndex": "1", "snmpProtocol": "FOURDOT1", "rsuUsername": "username", "rsuPassword": "password"}',
+        headers={'Content-Type': 'application/json'},
+        params={'index': '1'}
+    )
+
