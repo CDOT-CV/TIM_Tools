4e364b82b5ba35ab659bc2b8346c0f9518e4b6fe
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2024-12-17 12:49:16 -0700

    Add tim_translator unit tests

--- tests/Translators/RoadConditions/__init__.py
+++ tests/Translators/RoadConditions/__init__.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-# Add the WZDx directory to the path so that relative imports work
+# Add the RoadConditions directory to the path so that relative imports work
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'WZDx'))
+sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'RoadConditions'))
