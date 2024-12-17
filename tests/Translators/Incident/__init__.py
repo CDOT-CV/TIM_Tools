22a03dc1a91f93c7f3dd4ebe1c9ef1707776d485
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2024-12-17 13:43:45 -0700

    Add tests for tim_translator.py

--- tests/Translators/Incident/__init__.py
+++ tests/Translators/Incident/__init__.py
@@ -3,4 +3,4 @@ import os
 
 # Add the WZDx directory to the path so that relative imports work
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'WZDx'))
+sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'Incident'))
