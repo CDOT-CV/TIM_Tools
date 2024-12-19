4ad7ba169b13c42b3e3b517eb0b2d7a71f15b513
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2024-12-19 11:48:15 -0700

    Update __init__.py for WZDx translator

--- tests/Translators/WZDx/__init__.py
+++ tests/Translators/WZDx/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
-# Add the WZDx directory to the path so that relative imports work
+# Add both the WZDx and Shared directories to the path so that relative imports work
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'WZDx'))
+sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'Shared'))
