247bf38c5539de40cd25e97b760752cff29194bf
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-02-05 21:12:20 -0700

    Add weather stations feed translator

---
+++ Translators/WeatherStations/ws_tim_generator.py
@@ -0,0 +1,47 @@
+import os
+from itis_codes import ItisCodes
+from sensor_values import sensor_values, WIND_SPEED_CONSTANT
+import re
+
+def get_itis_codes(road_surface_sensor, wind_gust_sensor, avg_wind_speed_sensor):
+    itis_codes = []
+
+    # Only set ITIS codes if the road surface sensor is present
+    if len(road_surface_sensor) > 0:
+        rs_value = sensor_values.get(road_surface_sensor[0]["currentReading"], None)
+        if re.search(r"snow", rs_value, re.IGNORECASE):
+            itis_codes.append(ItisCodes.SNOW.value)
+        elif re.search(r"rain", rs_value, re.IGNORECASE):
+            itis_codes.append(ItisCodes.RAIN.value)
+            itis_codes.append(ItisCodes.WET_PAVEMENT.value)
+        elif re.search(r"frozen", rs_value, re.IGNORECASE):
+            itis_codes.append(ItisCodes.ICE.value)
+
+    # Only set ITIS codes if the average wind speed sensor is over threshold
+    if len(avg_wind_speed_sensor) == 1:
+        avg_wind_speed_value = avg_wind_speed_sensor[0]["currentReading"]
+        mph_wind_speed_value = float(avg_wind_speed_value) * WIND_SPEED_CONSTANT
+        mph_wind_speed_threshold = float(os.getenv("HIGH_WIND_THRESHOLD", 20))
+        if mph_wind_speed_value >= mph_wind_speed_threshold:
+            itis_codes.append(ItisCodes.STRONG_WINDS.value)
+
+    # Set ITIS codes if the wind gust sensor is over threshold and strong winds are not already set
+    if len(wind_gust_sensor) == 1 and ItisCodes.STRONG_WINDS.value not in itis_codes:
+        wind_gust_value = wind_gust_sensor[0]["currentReading"]
+        mph_gust_value = float(wind_gust_value) * WIND_SPEED_CONSTANT
+        mph_gust_threshold = float(os.getenv("HIGH_WIND_THRESHOLD", 20))
+        if mph_gust_value >= mph_gust_threshold:
+            itis_codes.append(ItisCodes.STRONG_WINDS.value)
+
+    return itis_codes
+
+def get_geometry(geometry):
+    annotated_geometry = []
+    # handle point geometry
+    if type(geometry[0]) == float:
+        annotated_geometry.append({"latitude": geometry[1], "longitude": geometry[0]})
+        return annotated_geometry
+    
+    for coord in geometry:
+        annotated_geometry.append({"latitude": coord[1], "longitude": coord[0]})
+    return annotated_geometry
---
+++ Translators/WeatherStations/ws_tim_translator.py
@@ -0,0 +1,69 @@
+import logging
+from pgquery import query_db
+from active_tim import active_tim
+from ws_tim_generator import get_geometry, get_itis_codes
+
+class WeatherStationFeature:
+    def __init__(self, properties, geometry):
+        self.id = properties["id"].replace("_", "-")
+        self.route = properties["routeName"].replace("_", "-")
+        self.geometry = geometry
+        self.direction = properties["direction"]
+        self.surface_status_sensor = [sensor for sensor in properties["sensors"] \
+                                        if sensor["type"] is not None and sensor["type"].lower() == "road surface status"]
+        self.wind_gust_sensor = [sensor for sensor in properties["sensors"] \
+                                        if sensor["type"] is not None and sensor["type"] == "gust wind speed"]
+        self.avg_wind_speed_sensor = [sensor for sensor in properties["sensors"] \
+                                        if sensor["type"] is not None and sensor["type"] == "average wind speed"]
+
+    def get_id(self):
+        return self.id
+    
+    def get_client_id(self):
+        return self.id.replace("/", "-")
+    
+    def get_route(self):
+        return self.route
+    
+    def get_geometry(self):
+        return self.geometry
+    
+    def get_direction(self):
+        return self.direction
+    
+    def get_surface_status_sensor(self):
+        return self.surface_status_sensor
+    
+    def get_wind_gust_sensor(self):
+        return self.wind_gust_sensor
+    
+    def get_avg_wind_speed_sensor(self):
+        return self.avg_wind_speed_sensor
+    
+def calculate_direction(direction):
+    if (direction.lower() == "n" or direction.lower() == "e"):
+        return "I"
+    else:
+        return "D"
+
+def translate(ws_geojson):
+    tims = {"timRcList": []}
+
+    for feature in ws_geojson["features"]:
+        feature = WeatherStationFeature(feature["properties"], feature["geometry"]["coordinates"])
+        tim_body = {}
+        tim_body["clientId"] = feature.get_client_id()
+        tim_body["direction"] = calculate_direction(feature.get_direction())
+        tim_body["route"] = feature.get_route()
+        tim_body["roadCode"] = feature.get_id()
+        tim_body["itisCodes"] = get_itis_codes(feature.get_surface_status_sensor(), \
+                                                feature.get_wind_gust_sensor(), \
+                                                feature.get_avg_wind_speed_sensor())
+        tim_body["geometry"] = get_geometry(feature.get_geometry())
+        tim_body["advisory"] = []
+        active_tim_record = active_tim("RC", tim_body)
+        if active_tim_record:
+            logging.info(f"TIM already active for record: {tim_body['clientId']}")
+            continue
+        tims["timRcList"].append(tim_body)
+    return tims
