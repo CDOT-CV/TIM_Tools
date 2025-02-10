da7c1d8c793279b9f63d832c9cc9aa2975ac34f8
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-02-10 09:46:52 -0700

    Add shared directory to test __init__ file

--- tests/Translators/Incident/__init__.py
+++ tests/Translators/Incident/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
-# Add the Incident directory to the path so that relative imports work
+# Add the Incident and Shared directories to the path so that relative imports work
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'Shared'))
 sys.path.append(os.path.join(SCRIPT_DIR, '..', '..', '..', 'Translators', 'Incident'))
