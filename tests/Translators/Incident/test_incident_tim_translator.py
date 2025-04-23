f7933aa520733ecb445163ffd4eb79f9d94c5bb2
Marc Wodahl <56242265+mwodahl@users.noreply.github.com>
2025-04-23 13:47:47 -0600

    Remove duplicate active_tim test

--- tests/Translators/Incident/test_incident_tim_translator.py
+++ tests/Translators/Incident/test_incident_tim_translator.py
@@ -62,25 +62,3 @@ def test_translate(mock_calculate_direction, mock_get_itis_codes, mock_get_point
     assert result["timIncidentList"][1]["startPoint"] == {"latitude": 0.0, "longitude": 100.0}
     assert result["timIncidentList"][1]["endPoint"] == {"latitude": 1.0, "longitude": 101.0}
     assert result["timIncidentList"][1]["direction"] == "N"
-
-@patch('Translators.Incident.incident_tim_translator.query_db')
-def test_active_tim_active(mock_query_db):
-    tim_body = {
-        "clientId": "incident-1",
-        "direction": "N",
-        "geometry": [
-            {"latitude": 0.0, "longitude": 100.0},
-            {"latitude": 1.0, "longitude": 101.0}
-        ]
-    }
-
-    mock_query_db.return_value = [{
-        "direction": "N",
-        "start_latitude": 0.0,
-        "start_longitude": 100.0,
-        "end_latitude": 1.0,
-        "end_longitude": 101.0
-    }]
-
-    result = tim_translator.active_tim(tim_body)
-    assert result is True
